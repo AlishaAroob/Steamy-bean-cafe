86f8501ca10b20535e26052216039b8d85ea31bc
AlishaAroob <aroob.dev@gmail.com>
2024-02-26 21:15:04 +0500

    styling reviews

--- coffee.js
+++ coffee.js
@@ -1,14 +1,1 @@
-let slideIndex = 0;
-function moveSlide(n) {
-  const slide = document.querySelectorAll(".slide");
-  slideIndex += n;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  } else if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
-  const slideWidth = slides[0].clientWidth;
-  document.querySelector(".slider").style.transform = `translateX(-${
-    slideWidth * slideIndex
-  }px)`;
-}
+
