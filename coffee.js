1e9c4c6a897199d76b31321b2dd8070389e274cb
AlishaAroob <aroob.dev@gmail.com>
2024-02-27 21:37:23 +0500

    styling review

--- coffee.js
+++ coffee.js
@@ -1,4 +1,3 @@
-let swiper = document.querySelector(".slider-container .slider");
 let slider = document.querySelectorAll(".slider .slide");
 let prev = document.getElementsByClassName("prev");
 let next = document.getElementsByClassName("next");
