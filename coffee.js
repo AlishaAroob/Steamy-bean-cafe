beb0de58baf2d53cdd38876023d7a27071b4413c
AlishaAroob <aroob.dev@gmail.com>
2024-02-27 11:36:56 +0500

    styling review

--- coffee.js
+++ coffee.js
@@ -2,3 +2,18 @@ let swiper = document.querySelector(".slider-container .slider");
 let slider = document.querySelectorAll(".slider .slide");
 let prev = document.getElementsByClassName("prev");
 let next = document.getElementsByClassName("next");
+
+let active = 0;
+let lengthslider = slider.length - 1;
+next.onclick = function () {
+  if (active + 1 > slide.length) {
+    active = 0;
+  } else {
+    active = active + 1;
+  }
+  reloadslider();
+};
+function reloadslider() {
+  let checkleft = slider[active].offsetleft;
+  slider.style.left = -checkleft + "px";
+}
