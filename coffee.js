0176b72ea16b91126d088c31aa894b5e6b3ce194
AlishaAroob <aroob.dev@gmail.com>
2024-02-28 17:44:57 +0500

    styling web

--- coffee.js
+++ coffee.js
@@ -1,18 +1,1 @@
-let slider = document.querySelectorAll(".slider .slide");
-let prev = document.getElementsByClassName("prev");
-let next = document.getElementsByClassName("next");
 
-let active = 0;
-let lengthslider = slider.length - 1;
-next.onclick = function () {
-  if (active + 1 > slide.length) {
-    active = 0;
-  } else {
-    active = active + 1;
-  }
-  reloadslider();
-};
-function reloadslider() {
-  let checkleft = slider[active].offsetleft;
-  slider.style.left = -checkleft + "px";
-}
