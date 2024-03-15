ec7b53d3f9ad63f94a570016905080ae2aa2caf1
AlishaAroob <aroob.dev@gmail.com>
2024-03-15 15:00:05 +0500

    js

--- coffee.js
+++ coffee.js
@@ -1,23 +1,42 @@
-const slide = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
 const prev = document.querySelector(".prev");
 const next = document.querySelector("next");
+
+let slideIndex = 0;
+
+let previous = document.getElementById("prev");
+previous.addEventListener("click", () => {
+  slideIndex = slideIndex - 1;
+  //   slideIndex = slideIndex > 0 ? slideIndex - 1 : 0;
+  //   slider.style.transform = `translateX(${slideIndex * -100}%)`;
+  slider.style.transform = "translateX(-100%)";
+});
+
+let nextt = document.getElementById("next");
+nextt.addEventListener("click", () => {
+  slideIndex = slideIndex + 1;
+  // slideIndex < slides.length - 1 ? slideIndex + 1 : slides.length - 1;
+  //   slider.style.transform = `translateX(${slideIndex * -100}%)`;
+  slider.style.transform = "translateX(-100%)";
+});
 
 // let swiper = document.getElementsByClassName("slide");
 // console.log(swiper);
 // slide[0].style.background = "yellow";
-let slides = document.getElementsByClassName("slide");
-console.log(slides);
-slide[1].style.transform = "translateX(-100px)";
+// let slides = document.getElementsByClassName("slide");
+// console.log(slides);
+// slide[1].style.transform = "translateX(-100px)";
 
-let nextt = document.getElementById("next");
-console.log(nextt);
+// let nextt = document.getElementById("next");
+// console.log(nextt);
 // nextt.style.background = "green";
 
-let slidenumber = 1;
-nextt.addEventListener(click, () => {
-  slides.style.transform = `transformX(-${slidenumber * 500}px)`;
-  slidenumber++;
-});
+// let slidenumber = 1;
+// nextt.addEventListener(click, () => {
+//   slides.style.transform = `transformX(-${slidenumber * 500}px)`;
+//   slidenumber++;
+// });
 
 // let flag = 1;
 // function click(x) {
